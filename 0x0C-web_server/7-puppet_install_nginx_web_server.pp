8087f036b324237b0da36504bb07a40e58ad9d31
1uiscalderon <1857@holbertonschool.com>
2020-12-09 19:56:31 -0500

    Install Nginx using puppet

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,19 @@
+# Configure an Nginx server using Puppet instead of Bash
+package { 'nginx':
+  ensure => installed,
+  name   => 'nginx',
+}
+file { 'index.html':
+  content => 'Holberton School',
+  path    => '/var/www/html/index.html'
+}
+file_line { 'append instruction':
+  ensure   => present,
+  path     => '/etc/nginx/sites-available/default',
+  after    => 'server_name _;',
+  line     => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+}
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
+}
