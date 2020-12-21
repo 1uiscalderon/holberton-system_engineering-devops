7ab601815e340125c39835f39154b47223ab8116
1uiscalderon <1857@holbertonschool.com>
2020-12-21 16:08:47 -0500

    Advanced task

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -18,7 +18,7 @@ file_line { 'Header instruction':
   ensure => present,
   path   => '/etc/nginx/sites-available/default',
   after  => 'server_name _;',
-  line   => 'add_header X-Served-By \$hostname;',   
+  line   => 'add_header X-Served-By "$HOSTNAME";',
 }
 service { 'nginx':
   ensure  => running,
