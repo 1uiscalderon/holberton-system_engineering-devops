109b1d2d1ba25c60c454e61dbc4bcd1419ea3fb8
1uiscalderon <1857@holbertonschool.com>
2020-12-21 16:00:24 -0500

    advanced task

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,26 @@
+# Configure an Nginx server using Puppet instead of Bash
+
+package { 'nginx':
+  ensure => installed,
+}
+
+file { 'index.html':
+  content => 'Holberton School',
+  path    => '/var/www/html/index.html'
+}
+file_line { 'append instruction':
+  ensure => present,
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'server_name _;',
+  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+}
+file_line { 'Header instruction':
+  ensure => present,
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'server_name _;',
+  line   => 'add_header X-Served-By \$hostname;',   
+}
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
+}
