4da0611d25595cdd7d9e748da27fa50a8b6c688e
1uiscalderon <1857@holbertonschool.com>
2020-12-21 16:57:26 -0500

    Advanced task

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -3,7 +3,7 @@
 exec { 'Header adition':
   command => 'sudo apt-get update -y;
   sudo apt-get install nginx -y;
-  sudo sed -i "/server_name _/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default
+  sudo sed -i "/server_name _/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default;
   sudo service nginx restart',
   provider => 'shell',
 }
