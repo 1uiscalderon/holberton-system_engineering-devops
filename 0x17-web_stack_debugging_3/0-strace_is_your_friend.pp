281eda5e1227773d79befcb471475b3ac2844f80
1uiscalderon <1857@holbertonschool.com>
2021-02-18 08:36:10 -0500

    Double quote fixed

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 #Change the extension .phpp to .php 
 exec { 'Header adition':
   command => 'sudo sed -i "s/phpp/php/" /var/www/html/wp-settings.php',
-  path    => ["/usr/bin", "/usr/sbin"],
+  path    => ['/usr/bin', '/usr/sbin'],
 }
